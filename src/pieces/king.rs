5edd04cc8b5500ef32e11d4498d176c7f504743f
Vinicius Borges <viniciusborges22@gmail.com>
2024-08-08 20:11:10 -0300

    Improve King's can_move() function

--- src/pieces/king.rs
+++ src/pieces/king.rs
@@ -1,4 +1,3 @@
-use crate::utils::constants::BOARD_SIZE;
 use crate::utils::Position;
 
 pub const SYMBOLS: [char; 2] = ['\u{2654}', '\u{265A}'];
@@ -7,24 +6,11 @@ pub fn can_move(origin: Position, destination: Position) -> bool {
     let (src_line, src_col) = (origin.line as i8, origin.col as i8);
     let (dest_line, dest_col) = (destination.line as i8, destination.col as i8);
 
-    // TODO this can be improved...
-    for i in -1..2_i8 {
-        for j in -1..2_i8 {
-            if i == 0 && j == 0 {
-                // Don't allow the King to stay where it is
-                continue;
-            }
-            if (src_line + i) < 0
-                || (src_line + i) >= BOARD_SIZE as i8
-                || (src_col + j) < 0
-                || (src_col + j) >= BOARD_SIZE as i8
-            {
-                continue;
-            }
-            if (src_line + i, src_col + j) == (dest_line, dest_col) {
-                return true;
-            }
-        }
+    let horizontal_distance = dest_col - src_col;
+    let vertical_distance = dest_line - src_line;
+
+    if (-1..=1).contains(&horizontal_distance) && (-1..=1).contains(&vertical_distance) {
+        return true;
     }
 
     false
