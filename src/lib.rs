172bf4a976750036e15b06cd7ac98529e7a2d6ad
Vinicius Borges <viniciusborges22@gmail.com>
2025-01-19 11:49:31 -0300

    Move logic from main.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,34 @@
 pub mod errors;
 mod game_state;
-pub mod io;
-pub mod pgn;
+mod io;
+mod pgn;
 mod piece;
 pub mod utils;
 
 pub use game_state::GameState;
+
+use crate::io::ui::read_move;
+
+pub fn run(game_state: &mut GameState) {
+    game_state.print_game();
+
+    loop {
+        match read_move() {
+            Ok(next_move) => {
+                if let Err(move_err) = game_state.handle_move(next_move.as_str()) {
+                    game_state.print_game();
+                    println!("{}", next_move);
+                    println!("{}", move_err);
+                    continue;
+                }
+            }
+            Err(move_err) => {
+                game_state.print_game();
+                println!("{}", move_err);
+                continue;
+            }
+        }
+
+        game_state.print_game();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use chess::GameState;
-use chess::io::ui::read_move;
+use chess::{run, GameState};
 
 fn main() {
     let mut game_state = GameState::new();
-
     game_state.initialize(None);
-    game_state.print_game();
-
-    loop {
-        match read_move() {
-            Ok(next_move) => {
-                if let Err(move_err) = game_state.handle_move(next_move.as_str()) {
-                    game_state.print_game();
-                    println!("{}", next_move);
-                    println!("{}", move_err);
-                    continue;
-                }
-            }
-            Err(move_err) => {
-                game_state.print_game();
-                println!("{}", move_err);
-                continue;
-            }
-        }
-
-        game_state.print_game();
-    }
+    run(&mut game_state);
 }
